6761977e752ac7d6334a3be1c8e789841b7f27d8
linjie <linjie@zhuanzhuan.com>
2021-02-04 15:07:02 +0800

    remote github

---
+++ examples/extend/app.ts
@@ -0,0 +1,31 @@
+import axios from '../../src/index'
+
+axios.get('/extend/get')
+
+axios.options('/extend/options')
+
+axios.delete('/extend/delete')
+
+axios.head('/extend/head')
+
+axios.post('/extend/post', { msg: 'post' })
+
+axios.put('/extend/put', { msg: 'put' })
+
+axios.patch('/extend/patch', { msg: 'patch' })
+
+
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+axios('/extend/post', {
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
